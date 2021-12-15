a205be7356d245f393b72f81705e416c6a31b52b
Shaikh Mohammed Shahnawaz <shaikhshahnawaz8821@gmail.com>
2021-12-15 18:13:36 +0530

    basic setup

--- src/components/Chatcomponent.js
+++ src/components/Chatcomponent.js
@@ -1,28 +1,29 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 import { io } from "socket.io-client";
-import Messagebody from './messages/Messagebody';
-import Messagefooter from './messages/Messagefooter';
-import Messageheader from './messages/Messageheader';
+import Messagebody from "./messages/Messagebody";
+import Messagefooter from "./messages/Messagefooter";
+import Messageheader from "./messages/Messageheader";
 
 const Chatcomponent = () => {
-    const socket = io("http://localhost:3001");
+  const socket = io("http://localhost:3001");
 
-    useEffect(() => {
-    
-        return () => {
-            
-        }
-    }, [])
+  useEffect(() => {
+    return () => {};
+  }, []);
 
+  return (
+    <div className="container-fluid row">
+      <div className="col-md-8 bg-info">
+        <Messageheader />
+        <Messagebody />
+        <Messagefooter />
+      </div>
 
-    return (
-        <>
-            <Messageheader/>
-            <Messagebody/>
-            <Messagefooter/>
+      <div className="col-md-4 bg-dark h-100">
+          <p>Hellow</p>
+      </div>
+    </div>
+  );
+};
 
-        </>
-    )
-}
-
-export default Chatcomponent
+export default Chatcomponent;
