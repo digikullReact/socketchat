ea6dd24f7172cee220c80536d5e225ed6820b71f
irutamboli <88967596+irutamboli@users.noreply.github.com>
2021-12-16 14:21:06 +0530

    Update Messageheader.jsx
    updating header section

--- src/components/messages/Messageheader.jsx
+++ src/components/messages/Messageheader.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import "./style/header.css"
 function Messageheader() {
+
     return (
-        <div className='bg-dark py-5 text-light'>
-             <h1>Header</h1>
+        <div id="header" className='d-flex justify-content-around align-items-center  py-2'>
+       
+
+          
+
+            <div className='d-flex  align-items-center'>
+            <img className='img-div rounded-circle' id="img" src="https://source.unsplash.com/100x100/?dog%E2%80%8B" ></img>
+
+            <p className='username ms-5' id="h4" ><h3>Diana<i  class="bi bi-dot online " ></i></h3>   
+            </p>
+            
+            </div> 
+            
+             <div>
+            <i id="time" class="bi bi-clock-history">&nbsp;&nbsp;  5 min ago</i>
+            </div>
+
+
         </div>
     )
 }
